5ba7eb57ad07cc8628c6eca7a6a5b1a21e91ccd6
Milos Vasic <office@milosvasic.net>
2016-10-21 13:35:43 +0200

    Work in progress - not usable still, just writing the code.

--- receiver.py
+++ receiver.py
@@ -7,71 +7,79 @@ import threading
 from termcolor import colored, cprint
 from common import pussycat_filter_file
 
-data = list()
-refreshing = False
-current_filter = ''
+
+class Receiver:
+    data = None
+    refreshing = None
+    current_filter = None
+
+    def __init__(self):
+        self.data = list
+        self.refreshing = False
+        self.current_filter = ''
+
+    def print_line(self, line):
+        out = None
+        if line.__contains__(" D/") or line.__contains__(" D "):
+            out = colored(line, 'grey')
+        if line.__contains__(" D/") or line.__contains__(" D "):
+            out = colored(line, 'white')
+        if line.__contains__(" I/") or line.__contains__(" I "):
+            out = colored(line, 'cyan')
+        if line.__contains__(" W/") or line.__contains__(" W "):
+            out = colored(line, 'yellow')
+        if line.__contains__(" E/") or line.__contains__(" E "):
+            out = colored(line, 'red')
+        cprint(out)
+
+    def receiver(self):
+        while True:
+            received = sys.stdin.readline().rstrip('\n')
+            if received:
+                self.data.append(received)
+                if self.filter_ok(received) and not self.refreshing:
+                    self.print_line(received)
+            else:
+                time.sleep(1)
+        return
+
+    def logcat_filter(self):
+        global current_filter
+        while True:
+            if not os.path.isfile(pussycat_filter_file):
+                if current_filter != '':
+                    current_filter = ''
+                    self.apply_filter()
+            else:
+                with open(pussycat_filter_file) as filter_file:
+                    filter_to_apply = filter_file.readlines()
+                    filter_file.close()
+                    if filter_to_apply == current_filter:
+                        pass
+                    current_filter = filter_to_apply
+                    self.apply_filter()
+        time.sleep(1)
+
+    def apply_filter(self):
+        refreshing = True
+        print(chr(27) + "[2J")
+        for item in self.data:
+            if self.filter_ok(item):
+                self.print_line(item)
+        refreshing = False
+
+    def filter_ok(self, reveived):
+        if current_filter != '':
+            pass
+        return True
+
+    def start(self):
+        receiver = threading.Thread(target=self.receiver())
+        receiver.start()
+
+        data_filter = threading.Thread(target=self.logcat_filter())
+        data_filter.start()
 
 
-def print_line(line):
-    if line.__contains__(" D/") or line.__contains__(" D "):
-        line = colored(line, 'grey')
-    if line.__contains__(" D/") or line.__contains__(" D "):
-        line = colored(line, 'white')
-    if line.__contains__(" I/") or line.__contains__(" I "):
-        line = colored(line, 'cyan')
-    if line.__contains__(" W/") or line.__contains__(" W "):
-        line = colored(line, 'yellow')
-    if line.__contains__(" E/") or line.__contains__(" E "):
-        line = colored(line, 'red')
-    cprint(line)
-
-
-def receiver():
-    while True:
-        received = sys.stdin.readline().rstrip('\n')
-        if received:
-            data.append(received)
-            if filter_ok(received) and not refreshing:
-                print_line(received)
-        else:
-            time.sleep(1)
-    return
-
-
-def logcat_filter():
-    global current_filter
-    while True:
-        if not os.path.isfile(pussycat_filter_file):
-            if current_filter != '':
-                current_filter = ''
-                apply_filter()
-        else:
-            with open(pussycat_filter_file) as filter_file:
-                filter_to_apply = filter_file.readlines()
-                filter_file.close()
-                if filter_to_apply == current_filter:
-                    pass
-                current_filter = filter_to_apply
-                apply_filter()
-    time.sleep(1)
-
-
-def apply_filter():
-    refreshing = True
-    print(chr(27) + "[2J")
-    for item in data:
-        if filter_ok(item):
-            print_line(item)
-    refreshing = False
-
-
-def filter_ok(reveived):
-    if current_filter != '':
-    return True
-
-
-receiver = threading.Thread(target=receiver())
+receiver = Receiver()
 receiver.start()
-
-data_filter = threading.Thread(target=logcat_filter())
-data_filter.start()
