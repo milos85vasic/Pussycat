ba5a5f79f339259db7a1686c1286296c335f7cea
Milos Vasic <milos85vasic@gmail.com>
2016-11-21 12:15:55 +0100

    Pussycat: Refactoring.

--- src/net/milosvasic/pussycat/Pussycat.kt
+++ src/net/milosvasic/pussycat/Pussycat.kt
@@ -1,21 +1,25 @@
 package net.milosvasic.pussycat
 
+import net.milosvasic.pussycat.color.Color
+import net.milosvasic.pussycat.core.LogcatTagType
 import net.milosvasic.pussycat.core.commands.COMMAND
 import java.util.concurrent.atomic.AtomicBoolean
 import net.milosvasic.pussycat.core.common.DataFilter
 import net.milosvasic.pussycat.core.data.Data
 import net.milosvasic.pussycat.logging.ConsoleLogger
 import net.milosvasic.pussycat.utils.Text
-import java.io.File
 import java.util.concurrent.CopyOnWriteArrayList
 import net.milosvasic.pussycat.core.common.Execute
+import java.io.*
 
 
 class Pussycat() : Execute<COMMAND, String>, DataFilter<CopyOnWriteArrayList<String>, String> {
 
     private val data = Data(this)
     private val TAG = Pussycat::class
+    private val run = AtomicBoolean()
     private var logger = ConsoleLogger()
+    private var color: String = Color.BLACK
     private val paused = AtomicBoolean(false)
     private var refreshing = AtomicBoolean(false)
 
@@ -28,23 +32,85 @@ class Pussycat() : Execute<COMMAND, String>, DataFilter<CopyOnWriteArrayList<Str
             COMMAND.RESET -> filter()
             COMMAND.PAUSE -> pause()
             COMMAND.RESUME -> resume()
-            COMMAND.STATUS -> logger.v(TAG, "--- PussycatLegacy, apply [ ${getFilter()} ] ---\n\n")
+            COMMAND.STATUS -> logger.v(TAG, "Filter applied [ ${getFilter()} ]\n\n")
             else -> logger.w(TAG, "Unknown command: " + executable)
         }
     }
 
-    fun live() {
-
-    }
-
-    fun filesystem(params: Array<String?>) {
+    private fun filesystem(params: Array<out String?>) {
         val logcat = File(params[0] as String)
         if (logcat.exists()) {
+            run.set(false)
+            Thread(Runnable {
+                Thread.currentThread().name = "Filesystem reading thread"
+                val input = FileInputStream(logcat.absoluteFile)
+                val reader = InputStreamReader(input)
+                val buffered = BufferedReader(reader)
+                var line = ""
 
+                while (run.get() && line != null) {
+                    try {
+                        line = buffered.readLine()
+                    } catch (e: Exception) {
+                        run.set(false)
+                    }
+                    if (!Text.isEmpty(line)) {
+                        line = line.trim()
+                        if (!Text.isEmpty(line)) {
+                            data.addData(line)
+                            if (!refreshing.get() && filterOk(line)) {
+                                printLine(line)
+                            }
+                        }
+                    }
+                }
+
+                buffered.close()
+                reader.close()
+                input.close()
+            }).start()
         } else {
-            logger.e(TAG, "Logcat: ${logcat.absolutePath} does not exist")
+            logger.e(TAG, "Logcat: ${logcat.absoluteFile} does not exist")
             stop()
         }
+    }
+
+    fun live() {
+        run.set(false)
+        Thread(Runnable {
+            Thread.currentThread().name = "Live adb reading thread"
+            val process = Runtime.getRuntime().exec("adb logcat")
+            val input = process.inputStream
+            var line = ""
+            val reader = InputStreamReader(input)
+            val buffered = BufferedReader(reader)
+
+            while (run.get()) {
+                try {
+                    line = buffered.readLine()
+                } catch (e: Exception) {
+                    run.set(false)
+                }
+                if (!Text.isEmpty(line)) {
+                    line = line.trim()
+                    if (!Text.isEmpty(line)) {
+                        data.addData(line)
+                        if (!refreshing.get() && filterOk(line)) {
+                            printLine(line)
+                        }
+                    } else {
+                        Thread.sleep(1000)
+                    }
+                } else {
+                    Thread.sleep(1000)
+                }
+            }
+
+            buffered.close()
+            reader.close()
+            input.close()
+            process.destroy()
+        }).start()
     }
 
     fun stop() {
@@ -79,9 +145,44 @@ class Pussycat() : Execute<COMMAND, String>, DataFilter<CopyOnWriteArrayList<Str
         execute(COMMAND.RESET)
     }
 
-
     override fun apply(data: CopyOnWriteArrayList<String>, pattern: String?) {
         // TODO: Implement this.
+    }
+
+    private fun printLine(line: String) {
+        if (paused.get()) {
+            return
+        }
+        if (line.containsIgnoreCase(LogcatTagType.V_LIVE) || line.containsIgnoreCase(LogcatTagType.V_FILESYSTEM)) {
+            color = Color.WHITE
+            println("$color$line${Color.RESET}")
+            return
+        }
+        if (line.containsIgnoreCase(LogcatTagType.D_LIVE) || line.containsIgnoreCase(LogcatTagType.D_FILESYSTEM)) {
+            color = Color.YELLOW
+            println("$color$line${Color.RESET}")
+            return
+        }
+        if (line.containsIgnoreCase(LogcatTagType.I_LIVE) || line.containsIgnoreCase(LogcatTagType.I_FILESYSTEM)) {
+            color = Color.CYAN
+            println("$color$line${Color.RESET}")
+            return
+        }
+        if (line.containsIgnoreCase(LogcatTagType.W_LIVE) || line.containsIgnoreCase(LogcatTagType.W_FILESYSTEM)) {
+            color = Color.PURPLE
+            println("$color$line${Color.RESET}")
+            return
+        }
+        if (line.containsIgnoreCase(LogcatTagType.E_LIVE) || line.containsIgnoreCase(LogcatTagType.E_FILESYSTEM)) {
+            color = Color.RED
+            println("$color$line${Color.RESET}")
+            return
+        }
+        println("\t$color$line${Color.RESET}")
+    }
+
+    fun String.containsIgnoreCase(word: String): Boolean {
+        return this.toLowerCase().contains(word.toLowerCase())
     }
 
 }
--- src/net/milosvasic/pussycat/core/data/Data.kt
+++ src/net/milosvasic/pussycat/core/data/Data.kt
@@ -1,5 +1,6 @@
 package net.milosvasic.pussycat.core.data
 
+import net.milosvasic.pussycat.core.LogcatTagType
 import java.util.concurrent.CopyOnWriteArrayList
 import net.milosvasic.pussycat.core.common.Filter
 import net.milosvasic.pussycat.core.common.DataFilter
@@ -16,6 +17,66 @@ class Data(val filter: DataFilter<CopyOnWriteArrayList<String>, String>) : Filte
 
     fun getFilterPattern(): String {
         return pattern
+    }
+
+    fun addData(line: String) {
+        val tag = getTag(line)
+        if (tag != null) {
+            val firstTagOccurance = line.indexOf(tag)
+            var splited = line.substring(firstTagOccurance + tag.length, line.lastIndex + 1)
+            val classEnd = splited.indexOf(":")
+            splited = splited.substring(classEnd + 1, splited.lastIndex + 1)
+            if (splited.startsWith(" \t")) {
+                data[data.lastIndex] = "${data.last()}\n$line"
+            } else {
+                data.add(line)
+            }
+        } else {
+            if (data.isEmpty()) {
+                data.add(line)
+            } else {
+                val replace = "${data.last()}\n\t$line"
+                data[data.lastIndex] = replace
+            }
+        }
+    }
+
+    private fun getTag(line: String): String? {
+        if (line.containsIgnoreCase(LogcatTagType.V_LIVE)) {
+            return LogcatTagType.V_LIVE
+        }
+        if (line.containsIgnoreCase(LogcatTagType.V_FILESYSTEM)) {
+            return LogcatTagType.V_FILESYSTEM
+        }
+        if (line.containsIgnoreCase(LogcatTagType.D_LIVE)) {
+            return LogcatTagType.D_LIVE
+        }
+        if (line.containsIgnoreCase(LogcatTagType.D_FILESYSTEM)) {
+            return LogcatTagType.D_FILESYSTEM
+        }
+        if (line.containsIgnoreCase(LogcatTagType.I_LIVE)) {
+            return LogcatTagType.I_LIVE
+        }
+        if (line.containsIgnoreCase(LogcatTagType.I_FILESYSTEM)) {
+            return LogcatTagType.I_FILESYSTEM
+        }
+        if (line.containsIgnoreCase(LogcatTagType.W_LIVE)) {
+            return LogcatTagType.W_LIVE
+        }
+        if (line.containsIgnoreCase(LogcatTagType.W_FILESYSTEM)) {
+            return LogcatTagType.W_FILESYSTEM
+        }
+        if (line.containsIgnoreCase(LogcatTagType.E_LIVE)) {
+            return LogcatTagType.E_LIVE
+        }
+        if (line.containsIgnoreCase(LogcatTagType.E_FILESYSTEM)) {
+            return LogcatTagType.E_FILESYSTEM
+        }
+        return null
+    }
+
+    fun String.containsIgnoreCase(word: String): Boolean {
+        return this.toLowerCase().contains(word.toLowerCase())
     }
 
 }
