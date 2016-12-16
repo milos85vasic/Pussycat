4f59ef0dab5ee91e74dcbeca60dd198cf0682ddc
Milos Vasic <milos85vasic@gmail.com>
2016-12-16 13:06:59 +0100

    Pussycat: migration to Gradle.

---
+++ Pussycat/src/test/kotlin/net/milosvasic/pussycat/test/SampleTest.kt
@@ -0,0 +1,15 @@
+package net.milosvasic.pussycat.test
+
+import kotlin.test.assertEquals
+import org.junit.Assert
+import org.junit.Test
+
+class SampleTest {
+
+
+    @Test
+    fun firstTest() {
+        Assert.assertTrue(1 == 1)
+    }
+
+}
