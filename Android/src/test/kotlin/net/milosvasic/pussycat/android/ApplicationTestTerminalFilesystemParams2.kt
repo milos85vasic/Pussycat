362afb1a15e09c98b722a4117d622a7750bf3302
Milos Vasic <milos85vasic@gmail.com>
2016-12-19 11:15:37 +0100

    Pussycat: Working on application test.

--- Android/src/test/kotlin/net/milosvasic/pussycat/android/ApplicationTestTerminalFilesystemParams2.kt
+++ Android/src/test/kotlin/net/milosvasic/pussycat/android/ApplicationTestTerminalFilesystemParams2.kt
@@ -5,10 +5,27 @@ import net.milosvasic.pussycat.application.APPLICATION_TYPE
 import org.junit.Assert
 import org.junit.Test
 
-class ApplicationTestTerminalFilesystemParams1 : ApplicationTestAbstract() {
+
+class ApplicationTestTerminalFilesystemParams2 : ApplicationTestAbstract() {
+
+    /*
+
+     init {
+//        val param6 = TestParameter(arrayOf("--terminal --filesystem==="), APPLICATION_TYPE.CLI)
+        val param7 = TestParameter(arrayOf("--terminal --filesystem=samples/android/testing_parser.txt"), APPLICATION_TYPE.CLI)
+        params.add(param1)
+        params.add(param2)
+        params.add(param3)
+        params.add(param4)
+//        params.add(param5)
+//        params.add(param6)
+        params.add(param7)
+    }
+
+    */
 
     init {
-        params = arrayOf("--terminal", "--filesystem")
+        params = arrayOf("--terminal", "--filesystem=")
         expectedType = APPLICATION_TYPE.CLI
     }
 
