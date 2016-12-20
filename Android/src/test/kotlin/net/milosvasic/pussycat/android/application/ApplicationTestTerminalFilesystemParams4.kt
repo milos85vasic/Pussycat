c6a4326e23bd284974b671e8076cf4f5767c5c42
Milos Vasic <milos85vasic@gmail.com>
2016-12-20 10:03:47 +0100

    Pussycat:  Test fixed.

--- Android/src/test/kotlin/net/milosvasic/pussycat/android/application/ApplicationTestTerminalFilesystemParams4.kt
+++ Android/src/test/kotlin/net/milosvasic/pussycat/android/application/ApplicationTestTerminalFilesystemParams4.kt
@@ -7,9 +7,13 @@ import net.milosvasic.pussycat.application.PUSSYCAT_MODE
 class ApplicationTestTerminalFilesystemParams4 : ApplicationTestAbstract() {
 
     init {
-        params = arrayOf("--terminal", "--filesystem=${localSample.absolutePath}")
         expectedType = APPLICATION_TYPE.CLI
         expectedMode = PUSSYCAT_MODE.FILESYSTEM
+    }
+
+    override fun testApplication() {
+        params = arrayOf("--terminal", "--filesystem=${localSample.absolutePath}")
+        super.testApplication()
     }
 
 }
