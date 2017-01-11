8bcad492065567c40052faff472eee5ccd95ebd8
Milos Vasic <milos85vasic@gmail.com>
2017-01-11 10:59:09 +0100

    Pussycat:  Working on logic test.

--- Android/src/test/kotlin/net/milosvasic/pussycat/android/logic/LogicTest.kt
+++ Android/src/test/kotlin/net/milosvasic/pussycat/android/logic/LogicTest.kt
@@ -2,23 +2,36 @@ package net.milosvasic.pussycat.android.logic
 
 import net.milosvasic.pussycat.PussycatAbstract
 import net.milosvasic.pussycat.android.application.Application
-import net.milosvasic.pussycat.android.terminal.BlackHoleTerminalPrinter
 import net.milosvasic.pussycat.application.APPLICATION_TYPE
 import net.milosvasic.pussycat.application.PUSSYCAT_MODE
+import net.milosvasic.pussycat.terminal.Printer
 import net.milosvasic.pussycat.utils.Files
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import java.io.File
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
 
 class LogicTest {
 
+    val linesPrinted = AtomicInteger()
+    val testSets = HashMap<String, Int>()
     var resources = mutableListOf<String>()
+
+    val printLineCallback = object : PrintLineCallback {
+        override fun printLine(text: String?) {
+            val got = linesPrinted.incrementAndGet()
+            println("[ $got ] >> $text")
+        }
+    }
 
     @Before
     fun beforeTestLogic() {
+        testSets.put("Lion", 2)
+
         resources.addAll(Files.getResourceFiles("samples/android/logic"))
         Assert.assertTrue(resources.size == 1)
         val root = PussycatAbstract.getPussycatHome()
@@ -43,13 +56,21 @@ class LogicTest {
             val params = arrayOf("--terminal", "--filesystem=${resourceFile.absolutePath}")
             val app = Application(params)
             app.pussy?.configuration?.exitOnStop = false
-//            app.pussy?.configuration?.terminalPriner = BlackHoleTerminalPrinter()
+            app.pussy?.configuration?.terminalPriner = TestLogicTerminalPrinter(printLineCallback)
             app.pussy?.configuration?.waitingForDevicesTimeoutInSeconds = 1
             Thread(Runnable {
                 app.start()
                 Assert.assertEquals(app.type, APPLICATION_TYPE.CLI)
             }).start()
-            Thread.sleep(3000)
+            Thread.sleep(1500)
+
+            for ((key, value) in testSets) {
+                linesPrinted.set(0)
+                app.pussy?.filter(key)
+                Assert.assertEquals(value + 1, linesPrinted.get())
+                linesPrinted.set(0)
+            }
+
             Assert.assertEquals(app.pussy?.getPussycatMode(), PUSSYCAT_MODE.FILESYSTEM)
             app.stop()
         }
@@ -64,6 +85,16 @@ class LogicTest {
             localSample.delete()
             Assert.assertFalse(localSample.exists())
         }
+    }
+
+    class TestLogicTerminalPrinter(val callback: PrintLineCallback) : Printer {
+        override fun printLine(text: String?) {
+            callback.printLine(text)
+        }
+    }
+
+    interface PrintLineCallback {
+        fun printLine(text: String?)
     }
 
 }
