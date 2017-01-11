f5df8c978b05207502400b3bacbd49b12d5a1e05
Milos Vasic <milos85vasic@gmail.com>
2017-01-11 10:16:23 +0100

    Pussycat:  Working on logic test.

--- Android/src/test/kotlin/net/milosvasic/pussycat/android/logic/LogicTest.kt
+++ Android/src/test/kotlin/net/milosvasic/pussycat/android/logic/LogicTest.kt
@@ -1,6 +1,10 @@
 package net.milosvasic.pussycat.android.logic
 
 import net.milosvasic.pussycat.PussycatAbstract
+import net.milosvasic.pussycat.android.application.Application
+import net.milosvasic.pussycat.android.terminal.BlackHoleTerminalPrinter
+import net.milosvasic.pussycat.application.APPLICATION_TYPE
+import net.milosvasic.pussycat.application.PUSSYCAT_MODE
 import net.milosvasic.pussycat.utils.Files
 import org.junit.After
 import org.junit.Assert
@@ -35,7 +39,19 @@ class LogicTest {
     @Test
     fun testLogic() {
         for (resource in resources) {
-
+            val resourceFile = File(PussycatAbstract.Companion.getPussycatHome(), resource)
+            val params = arrayOf("--terminal", "--filesystem=${resourceFile.absolutePath}")
+            val app = Application(params)
+            app.pussy?.configuration?.exitOnStop = false
+//            app.pussy?.configuration?.terminalPriner = BlackHoleTerminalPrinter()
+            app.pussy?.configuration?.waitingForDevicesTimeoutInSeconds = 1
+            Thread(Runnable {
+                app.start()
+                Assert.assertEquals(app.type, APPLICATION_TYPE.CLI)
+            }).start()
+            Thread.sleep(3000)
+            Assert.assertEquals(app.pussy?.getPussycatMode(), PUSSYCAT_MODE.FILESYSTEM)
+            app.stop()
         }
     }
 
