76b1e019902c66d06b2f583a6c8f3a0661ffa165
Milos Vasic <milos85vasic@gmail.com>
2017-01-12 12:33:28 +0100

    Pussycat: Docking icon added and support for macos.

--- Android/src/main/kotlin/net/milosvasic/pussycat/android/GuiPussycat.kt
+++ Android/src/main/kotlin/net/milosvasic/pussycat/android/GuiPussycat.kt
@@ -5,12 +5,16 @@ import net.milosvasic.pussycat.android.data.AndroidLogCatMessage
 import net.milosvasic.pussycat.android.gui.OnSplashComplete
 import net.milosvasic.pussycat.android.gui.PussycatSplashScreen
 import net.milosvasic.pussycat.application.ApplicationInformation
+import com.apple.eawt.Application;
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.JFrame
 
 
 class GuiPussycat(information: ApplicationInformation) : AndroidPussycat() {
 
     val mainFrame = JFrame()
+    private var favicon: BufferedImage? = null
 
     val splashScreenCallback: OnSplashComplete = object : OnSplashComplete {
         override fun onComplete(success: Boolean) {
@@ -22,8 +26,18 @@ class GuiPussycat(information: ApplicationInformation) : AndroidPussycat() {
     val splashScreen = PussycatSplashScreen(information, mainFrame, splashScreenCallback)
 
     init {
+        favicon = ImageIO.read(javaClass.classLoader.getResourceAsStream("icons/Favicon.png"))
         mainFrame.extendedState = JFrame.MAXIMIZED_BOTH
         mainFrame.isUndecorated = true
+        val osString = System.getProperty("os.name").toLowerCase()
+        if (osString.contains("mac")) {
+            val app = Application.getApplication()
+            app.dockIconImage = favicon
+        } else if (osString.contains("linux")) {
+
+        } else if (osString.contains("windows")) {
+
+        }
     }
 
     override fun start(args: Array<String>) {
