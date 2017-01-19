e7a75e2910b9b37d5b3f7c43664d8622e5a70a85
Milos Vasic <milos85vasic@gmail.com>
2017-01-19 09:17:30 +0100

    Pussycat: Added test menu item.

--- Pussycat/src/main/kotlin/net/milosvasic/pussycat/gui/PussycatMainMenuBar.kt
+++ Pussycat/src/main/kotlin/net/milosvasic/pussycat/gui/PussycatMainMenuBar.kt
@@ -13,12 +13,14 @@ class PussycatMainMenuBar(theme: Theme, width: Int, height: Int) : PussycatMenuB
     val t1 = PussycatMenuItem("T 1")
     val t2 = PussycatMenuItem("T 2")
     val t3 = PussycatMenuItem("T 3")
+    val t4 = PussycatMenuItem("T 4")
 
     init {
         file.addChild(t1)
         file.addChild(t2)
         help.addChild(about)
         help.addChild(t3)
+        help.addChild(t4)
         addChild(file)
         addChild(commands)
         addChild(help)
