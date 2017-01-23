a480147da6f177dca836d6696941f05d01e9ac9d
Milos Vasic <milos85vasic@gmail.com>
2017-01-23 10:45:13 +0100

    Pussycat: Small cleanup.

--- Pussycat/src/main/kotlin/net/milosvasic/pussycat/gui/PussycatPopupMenu.kt
+++ Pussycat/src/main/kotlin/net/milosvasic/pussycat/gui/PussycatPopupMenu.kt
@@ -4,7 +4,6 @@ import net.milosvasic.pussycat.gui.theme.Theme
 import net.milosvasic.pussycat.gui.theme.color.INTENSITY
 import net.milosvasic.pussycat.gui.theme.color.TYPE
 import java.awt.Graphics
-import java.awt.Toolkit
 import javax.swing.JPopupMenu
 
 
@@ -13,8 +12,6 @@ class PussycatPopupMenu(val theme: Theme) : JPopupMenu() {
     init {
         isOpaque = true
         background = theme.getColor(TYPE.BASE, INTENSITY.MEDIUM)
-        val screenSize = Toolkit.getDefaultToolkit().screenSize
-        preferredSize.width = screenSize.width
     }
 
     override fun paintComponent(g: Graphics?) {
