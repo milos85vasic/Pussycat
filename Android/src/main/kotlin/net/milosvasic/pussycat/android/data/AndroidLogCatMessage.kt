e8e340bd352d7a6d8c45ace11adcaea539939860
Milos Vasic <milos85vasic@gmail.com>
2017-01-25 14:13:18 +0100

    Pussycat: Working on UI population.

--- Android/src/main/kotlin/net/milosvasic/pussycat/android/data/AndroidLogCatMessage.kt
+++ Android/src/main/kotlin/net/milosvasic/pussycat/android/data/AndroidLogCatMessage.kt
@@ -32,8 +32,8 @@ class AndroidLogCatMessage(
     init {
         val pidLen = "$pid".length
         if (pidLen > LENGTHS.PID) {
-            if (pidLen >= 12) {
-                LENGTHS.PID = 12
+            if (pidLen >= LENGTHS.SPACING_DEFAULT) {
+                LENGTHS.PID = LENGTHS.SPACING_DEFAULT
             } else {
                 LENGTHS.PID = pidLen
             }
@@ -41,32 +41,32 @@ class AndroidLogCatMessage(
 
         val tidLen = "$tid".length
         if (tidLen > LENGTHS.TID) {
-            if (tidLen >= 12) {
-                LENGTHS.TID = 12
+            if (tidLen >= LENGTHS.SPACING_DEFAULT) {
+                LENGTHS.TID = LENGTHS.SPACING_DEFAULT
             } else {
                 LENGTHS.TID = tidLen
             }
         }
 
         if (appName.length > LENGTHS.APP_NAME) {
-            if (appName.length >= 15) {
-                LENGTHS.APP_NAME = 15
+            if (appName.length >= LENGTHS.SPACING_LONG) {
+                LENGTHS.APP_NAME = LENGTHS.SPACING_LONG
             } else {
                 LENGTHS.APP_NAME = appName.length
             }
         }
 
         if (tag.length > LENGTHS.TAG) {
-            if (tag.length >= 15) {
-                LENGTHS.TAG = 15
+            if (tag.length >= LENGTHS.SPACING_LONG) {
+                LENGTHS.TAG = LENGTHS.SPACING_LONG
             } else {
                 LENGTHS.TAG = tag.length
             }
         }
 
         if (time.length > LENGTHS.TIME) {
-            if (time.length >= 15) {
-                LENGTHS.TIME = 15
+            if (time.length >= LENGTHS.SPACING_LONG) {
+                LENGTHS.TIME = LENGTHS.SPACING_LONG
             } else {
                 LENGTHS.TIME = time.length
             }
@@ -78,6 +78,9 @@ class AndroidLogCatMessage(
     }
 
     class Lengths {
+        val SPACING_DEFAULT = 12
+        val SPACING_LONG = 15
+
         var PID = 0
         var TID = 0
         var APP_NAME = 0
