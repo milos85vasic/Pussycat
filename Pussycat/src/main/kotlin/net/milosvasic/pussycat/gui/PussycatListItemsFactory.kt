078ddf22dbbd29c2c2180618011c6dcfa20f2b2b
Milos Vasic <milos85vasic@gmail.com>
2017-01-27 13:48:57 +0100

    Pussycat: Optimization 1, Working on optimization - Limit to save processor power.

--- Pussycat/src/main/kotlin/net/milosvasic/pussycat/gui/PussycatListItemsFactory.kt
+++ Pussycat/src/main/kotlin/net/milosvasic/pussycat/gui/PussycatListItemsFactory.kt
@@ -3,32 +3,37 @@ package net.milosvasic.pussycat.gui
 import net.milosvasic.pussycat.gui.content.Labels
 import net.milosvasic.pussycat.listeners.Listener
 import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.atomic.AtomicInteger
 
 
 class PussycatListItemsFactory<T>(val factory: PussycatListItemFactory<T>) : Listener<Pair<T, Int>> {
 
+    val requested = AtomicInteger(100)
     var pollingThread: Thread? = null
     val queue = LinkedBlockingQueue<Pair<T, Int>?>()
-    val executor: ExecutorService = Executors.newFixedThreadPool(1)
     val data = ConcurrentHashMap<Int, PussycatListItem>()
 
     override fun onEvent(value: Pair<T, Int>?) {
         queue.add(value)
+        processData()
+    }
+
+    private fun processData() {
         if (pollingThread == null) {
             pollingThread = Thread(Runnable {
                 Thread.currentThread().name = Labels.POLLING_THREAD
-                while (!Thread.currentThread().isInterrupted && !queue.isEmpty()) {
+                while (!Thread.currentThread().isInterrupted && !queue.isEmpty() && data.size <= requested.get()) {
                     val polledItem = queue.poll()
                     val item = polledItem?.first
                     val index = polledItem?.second
                     if (item != null && index != null) {
-                        executor.submit {
-                            val view = factory.obtain(item, index)
-                            data.put(index, view)
-                        }
+                        println("start [ $index ]")
+                        val view = factory.obtain(item, index)
+                        view.isVisible = true
+                        view.validate()
+                        data.put(index, view)
+                        println("end [ $index ][ ${data.size} ]")
                     }
                 }
                 pollingThread = null
